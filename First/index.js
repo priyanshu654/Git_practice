5a29f8cd6825d9d43b99e9405ba5e981a8612cb9
Priyanshu Raj <priyanshubkj10@gmail.com>
2025-08-10 16:42:40 +0530

    Initial commit

---
+++ First/index.js
@@ -0,0 +1,10 @@
+let a=10;
+let b=20
+
+let c;
+
+function sum(a,b){
+    return a+b
+}
+
+c=sum(a,b);
---
+++ Second/second.js
@@ -0,0 +1,9 @@
+console.log("We are here in second file");
+
+let a=10;
+let b=5;
+
+function sub(a,b){
+    return a-b;
+}
+let c=sub(a,b);
